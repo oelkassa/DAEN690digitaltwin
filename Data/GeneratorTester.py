94a7e3b68e2853485878b22901bd3488d0133145
jhoffs <73670820+jhoffs@users.noreply.github.com>
2022-06-06 18:26:45 -0400

    Sample Dataset CSV created
    Created daset in CSV form

--- Data/GeneratorTester.py
+++ Data/GeneratorTester.py
@@ -8,6 +8,9 @@ import numpy as np
 
 gen = gen.DataGen()
 test = gen.generate(1000)
+test2 = gen.generateCSV("SampleData.csv", 1000)
+
+
 plt.scatter(test['k'], test['x_t'], label = "x(t)")
 plt.scatter(test['k'], test['Fatigue']/max(test['Fatigue']), label = "Fatigue")
 plt.xlabel("k")
