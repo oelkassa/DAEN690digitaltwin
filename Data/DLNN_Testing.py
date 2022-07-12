a9db881f422f3e1bc4746d53400f9a5170857d6a
jhoffs <73670820+jhoffs@users.noreply.github.com>
2022-07-12 18:04:48 -0400

    Update DLNN_Testing.py

--- Data/DLNN_Testing.py
+++ Data/DLNN_Testing.py
@@ -1,62 +1,77 @@
+# -*- coding: utf-8 -*-
 """
-Created on Mon Jun  6 14:19:28 2022
+Created on Tue Jul 12 17:57:57 2022
+
 @author: johnn
 """
-
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import DigitalTwinDataGenerator as gen
-import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn import metrics as sk
 
-#Create DigitalTwinDataGenerator object
-gen = gen.DataGen()  
+def createNN(x_train, y_train, iterations, numLyaers, numNodes, opt):
+    xNorm = np.array(x_train)
+    normLayer = keras.layers.Normalization(input_shape=[2,], axis=None)
+    normLayer.adapt(xNorm) 
+    #defines number of nodes for the hidden layers
+    #Create model for NN
+    model = tf.keras.Sequential([
+      normLayer,
+      tf.keras.layers.Dense(numNodes, activation = 'relu'),
+      tf.keras.layers.Dense(numNodes, activation = 'relu'),
+      tf.keras.layers.Dense(numNodes, activation = 'relu'), 
+      tf.keras.layers.Dense(numNodes, activation = 'relu'), 
+      tf.keras.layers.Dense(numNodes, activation = 'relu'),
+      tf.keras.layers.Dense(1)])
+    
+    model.compile(loss='mean_absolute_error', optimizer= opt)
+    model.fit(x_train, y_train, epochs = iterations, verbose = 0)
+    return model
 
-#Use gen to generate datset 
-data = gen.generate(1000, 0, 2.9)
+def testHyperParams(numLayers, numNodes, epochs, numTests, kLower, kUpper, lr):
+    dataMin = []
+    dataMax = []
+    dataMean = []
+    dataMedian = []
+    
+    errorMin = []
+    errorMax = []
+    errorMean = []
+    errorMedian = []
+    mse = []
+    
+    dataGenerator = gen.DataGen()
+    for i in range (0, numTests):
+        #Use gen to generate datset.
+        data = dataGenerator.generate(1000, kLower, kUpper)
+                
+        #Create x and y inputs for NN
+        x = pd.DataFrame({'k':data.k, 'x_0':data.x_zero})
+        y = data.x_t        
+            
+        #Split x and y into train and test
+        x_train,x_test, y_train, y_test = train_test_split(x,y,test_size=0.2, random_state=42)
+        
+        model = createNN(x_train, y_train, epochs, numLayers, numNodes, keras.optimizers.Adam(learning_rate = lr))
+        yPred = model.predict(x_test)
+        error = abs(np.array(yPred[:,0]) - np.array(y_test))
+        
+        #Calculate data stats
+        dataMin.append((min(np.array(data.k))))
+        dataMax.append((max(np.array(data.k))))
+        dataMean.append((np.mean(np.array(data.k))))
+        dataMedian.append((np.median(np.array(data.k))))        
+        errorMin.append(min(error))
+        errorMax.append(max(error))
+        errorMean.append(np.mean(error))
+        errorMedian.append(np.median(error))
+        mse.append(sk.mean_squared_error(y_test, yPred))        
+    result = pd.DataFrame({"dataMax":dataMax, "dataMean":dataMean,"dataMin":dataMin, "dataMedian":dataMedian, 
+                           "errorMax":errorMax, "errorMean":errorMean,"errorMin":errorMin, "errorMedian":errorMedian,
+                           "Mse":mse})
+    return result
 
-#Create x and y inputs for NN
-x = pd.DataFrame(data.k)
-x.insert(1, "x_zero", data.x_zero, True)
-
-y = pd.DataFrame(data.x_t)
-y.insert(1, "Fatigue", data.Fatigue, True)
-
-#Split x and y into train and test
-x_train,x_test, y_train, y_test, = train_test_split(x,y,test_size=0.2, random_state=42)
-xNorm = np.array(x_train)
-normLayer = keras.layers.Normalization(input_shape=[2,], axis=None)
-normLayer.adapt(xNorm)
-
-#defines number of nodes for the hidden layers
-numNodes = 32
-
-#Create model for NN
-model = tf.keras.Sequential([
-  normLayer,
-  tf.keras.layers.Dense(numNodes, activation = 'relu'),
-  tf.keras.layers.Dense(numNodes, activation = 'relu'),
-  tf.keras.layers.Dense(2)])
-
-model.compile(loss='mean_absolute_error', optimizer=keras.optimizers.Adam(0.001))
-fit = model.fit(x_train, y_train, epochs = 10)
-
-#create graphs for the testing performance of x(t) and Fatigue
-yPred = model.predict(x_test)
-figure, axis = plt.subplots(2,1)
-
-
-axis[0].scatter(x_test["k"], y_test["x_t"], label='Data')
-axis[0].scatter(x_test["k"], yPred[0:,0], color='k', label='Prediction')
-axis[0].set_title("DLNN for K Less Than Three")
-axis[0].legend()
-axis[0].grid(True)
-
-axis[1].scatter(x_test["k"], y_test["Fatigue"], label='Data')
-axis[1].scatter(x_test["k"], yPred[0:,1], color='k', label='Prediction')
-axis[1].legend()
-axis[1].grid(True)
-
-plt.show()
+result = testHyperParams(5, 128, 100, 5, 0, 4, .001)
