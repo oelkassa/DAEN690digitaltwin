857ce735cca4ef1fa180d78729f053e2128659dc
jhoffs <73670820+jhoffs@users.noreply.github.com>
2022-07-18 17:18:30 -0400

    Updated Data Gen and sample datasets
    Updated Data Gen and sample datasets

--- Data/DigitalTwinDataGenerator.py
+++ Data/DigitalTwinDataGenerator.py
@@ -89,3 +89,32 @@ class DataGen:
                 i+=1
             l+=1
         return output
+    
+    def generateIncrementalDataCSV(self, fileName, kLower, kUpper, instances, increment):
+        f = open(fileName, 'w+')
+        writer = csv.writer(f)
+        writer.writerow(self.fields)
+        l = 0
+        while l < instances:
+            i = 0
+            n = (kUpper - kLower)/increment 
+            k = kLower
+            while i < n:
+                j = 0
+                x_t = random.random() # x_t is x(t)
+                x_zero = x_t
+                fat = 0     #fat is the fatigue
+                compA = 1 - x_t  #compA is xA(t)
+                compB = x_t * k  #compB is xB(t)
+                while j < 100:
+                    compA = 1 - x_t
+                    compB = x_t * k
+                    fat = fat + abs(compA*compB - x_t)
+                    x_t = compA * compB
+                    j += 1  
+                row = [round(k, self.sigFig), round(x_zero, self.sigFig), round(x_t, self.sigFig), round(compA, self.sigFig), round(compB, self.sigFig), round(fat, self.sigFig)]
+                writer.writerow(row)
+                k += increment
+                i+=1
+            l+=1
+        f.close()
